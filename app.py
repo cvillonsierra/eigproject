cf89fe79893f293380c360bf2f2b3a43c7d7e0ed
cvillonsierra <carlos_vs@outlook.com>
2021-08-23 23:34:10 -0500

    yfinance

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/quote")
+def display_quote():
+  symbol = request.args.get('symbol', default="AAPL")
+
+  quote = yf.Ticker(symbol)
+
+  return quote.info
+
+if __name__ == "___main___":
+  app.run(debug=True)
