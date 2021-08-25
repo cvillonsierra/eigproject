1cc0219fc98a0b75beab398a7b53dcf464367365
cvillonsierra <carlos_vs@outlook.com>
2021-08-24 19:04:00 -0500

    updates

--- app.py
+++ app.py
@@ -3,14 +3,29 @@ import yfinance as yf
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("")
 
 @app.route("/quote")
 def display_quote():
-  symbol = request.args.get('symbol', default="AAPL")
+    symbol = request.args.get('symbol', default="AAPL")
 
-  quote = yf.Ticker(symbol)
+    quote = yf.Ticker(symbol)
 
-  return quote.info
+    return quote.info
 
-if __name__ == "___main___":
-  app.run(debug=True)
+
+@app.route("/history")
+def display_history():
+    symbol = request.args.get('symbol', default="AAPL")
+    period = request.args.get('period', default="1y")
+    interval = request.args.get('interval', default="1mo")
+    quote = yf.Ticker(symbol)
+    hist = quote.history(period=period, interval=interval)
+    data = hist.to_json()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
